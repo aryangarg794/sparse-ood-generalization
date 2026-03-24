9d022a547880ee86f1fe24c0756824d4337be959
Aryan Garg <aryangarg794@gmail.com>
2026-03-24 17:54:52 +0100

    data generation script

--- src/sparse_generalization/data/shapes/shape_dataset.py
+++ src/sparse_generalization/data/shapes/shape_dataset.py
@@ -1,12 +1,16 @@
+import dill
 import numpy as np 
 import matplotlib.pyplot as plt
 import random
+import os
+import torch
 
-from sparse_generalization.data.shapes.constants import SHAPE_MAP, SHAPE_COLORS, SHAPES
+from matplotlib.backends.backend_pdf import PdfPages
+from sparse_generalization.data.shapes.constants import SHAPE_MAP, SHAPE_COLORS, SHAPES, SHAPES_TO_IDX
 
 def is_horizontal_adjacent(grid, shape1, shape2):
-    for r in range(3):
-        for c in range(2):
+    for r in range(grid.shape[0]):
+        for c in range(grid.shape[1]-1):
             if grid[r,c] == shape1 and grid[r,c+1] == shape2:
                 return True
             if grid[r,c] == shape2 and grid[r,c+1] == shape1:
@@ -27,7 +31,7 @@ def generate_grid(size=6, label_A=False, mode='train'):
             for shapeA, shapeB in [('heart', 'star'), ('circle', 'square')]: # each of the combos
                 posA = random.sample(positions, k=1)[0]
                 left_right = random.choice([-1, 1])
-                posB = (posA[0] + left_right, posA[0])
+                posB = (posA[0], posA[0] + left_right)
                 grid[posA[0], posA[1]] = shapeA
                 grid[posB[0], posB[1]] = shapeB
                 positions.remove(posA)
@@ -39,7 +43,7 @@ def generate_grid(size=6, label_A=False, mode='train'):
 
             posA = random.sample(positions, k=1)[0]
             left_right = random.choice([-1, 1])
-            posB = (posA[0] + left_right, posA[0])
+            posB = (posA[0], posA[0] + left_right)
             grid[posA[0], posA[1]] = shapeA
             positions.remove(posA)
             positions.remove(posB)
@@ -50,7 +54,7 @@ def generate_grid(size=6, label_A=False, mode='train'):
 
             posA = random.sample(positions, k=1)[0]
             left_right = random.choice([-1, 1])
-            posB = (posA[0] + left_right, posA[0])
+            posB = (posA[0], posA[0] + left_right)
             grid[posA[0], posA[1]] = shapeA
             positions.remove(posA)
             positions.remove(posB)
@@ -62,3 +66,128 @@ def generate_grid(size=6, label_A=False, mode='train'):
         idx += 1
     
     return grid
+
+def generate_dataset(
+    num_samples: int = 1000, 
+    val_size: int = 1000, 
+    test_size: int = 5000, 
+    visualize: bool = True, 
+    size: int = 6
+):
+    half_samples = num_samples // 2
+    half_test = test_size // 2
+    half_val = val_size // 2
+    dataset = {}
+    
+    # train
+    training_samples = []
+    training_labels = []
+    for label in [True, False]:
+        for _ in range(half_samples):
+            grid = generate_grid(size=size, label_A=label, mode='train')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            training_samples.append(inp_tensor)
+            training_labels.append(int(label))
+    
+    dataset['X_train'] = torch.stack(training_samples, dim=0)
+    dataset['Y_train'] = torch.stack(training_labels, dim=0)       
+        
+    # test (id)
+    test_samples = []
+    test_labels = []
+    for label in [True, False]:
+        for _ in range(half_test):
+            grid = generate_grid(size=size, label_A=label, mode='train')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            test_samples.append(inp_tensor)
+            test_labels.append(int(label))
+    
+    dataset['X_test'] = torch.stack(test_samples, dim=0)
+    dataset['Y_test'] = torch.stack(test_labels, dim=0)
+
+    # val (id)
+    val_samples = []
+    val_labels = []
+    for label in [True, False]:
+        for _ in range(half_val):
+            grid = generate_grid(size=size, label_A=label, mode='train')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            val_samples.append(inp_tensor)
+            val_labels.append(int(label))
+    
+    dataset['X_val'] = torch.stack(val_samples, dim=0)
+    dataset['Y_val'] = torch.stack(val_labels, dim=0)
+    
+    # test A
+    test_samples = []
+    test_labels = []
+    for label in [True, False]:
+        for _ in range(half_test):
+            grid = generate_grid(size=size, label_A=label, mode='test_a')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            test_samples.append(inp_tensor)
+            test_labels.append(int(label))
+    
+    dataset['X_test_a'] = torch.stack(test_samples, dim=0)
+    dataset['Y_test_a'] = torch.stack(test_labels, dim=0)
+    
+    # test B
+    test_samples = []
+    test_labels = []
+    for label in [True, False]:
+        for _ in range(half_test):
+            grid = generate_grid(size=size, label_A=label, mode='test_b')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            test_samples.append(inp_tensor)
+            test_labels.append(int(label))
+    
+    dataset['X_test_b'] = torch.stack(test_samples, dim=0)
+    dataset['Y_test_b'] = torch.stack(test_labels, dim=0)
+    
+    # val A
+    val_samples = []
+    val_labels = []
+    for label in [True, False]:
+        for _ in range(half_test):
+            grid = generate_grid(size=size, label_A=label, mode='test_a')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            val_samples.append(inp_tensor)
+            val_labels.append(int(label))
+    
+    dataset['X_val_a'] = torch.stack(val_samples, dim=0)
+    dataset['Y_val_a'] = torch.stack(val_labels, dim=0)
+    
+    # val B
+    val_samples = []
+    val_labels = []
+    for label in [True, False]:
+        for _ in range(half_test):
+            grid = generate_grid(size=size, label_A=label, mode='test_b')
+            inp_tensor = torch.zeros((size, size, 1), dtype=torch.int64)
+            for x in range(size):
+                for y in range(size):
+                    inp_tensor[x, y] = SHAPES_TO_IDX[grid[x, y]]
+            val_samples.append(inp_tensor)
+            val_labels.append(int(label))
+    
+    dataset['X_val_b'] = torch.stack(val_samples, dim=0)
+    dataset['Y_val_b'] = torch.stack(val_labels, dim=0)
+        
